7003673d66383bc207c903e1bb05afc844873aa6
Oleh <mihailyakyb45@gmail.com>
2024-12-14 07:05:20 +0200

    added fixes

--- src/main/java/car/sharing/app/carsharingservice/controller/AuthenticationController.java
+++ src/main/java/car/sharing/app/carsharingservice/controller/AuthenticationController.java
@@ -9,9 +9,11 @@ import car.sharing.app.carsharingservice.security.AuthenticationService;
 import car.sharing.app.carsharingservice.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +24,7 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
+    @ResponseStatus(HttpStatus.OK)
     public UserDto registerUser(@RequestBody @Valid UserRegistrationRequestDto userDto)
                                         throws RegistrationException {
         return userService.register(userDto);
